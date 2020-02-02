69460a05165389a5d7be221f0d95f43fdb0f7af1
AnumariAnnapoorna <anumari.annapoorna@gmail.com>
2020-02-01 23:07:01 -0500

    MavenDemo

---
+++ AnnapoornaMavenTask/src/main/java/MavenHomeTask/App.java
@@ -0,0 +1,13 @@
+package MavenHomeTask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Epam!" );
+    }
+}
